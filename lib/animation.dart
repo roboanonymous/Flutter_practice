afd89069bfb1c6af4cf9da9b0aa8c8c704c17832
roboanonymous <jsamyak591@gmail.com>
2023-05-18 11:04:15 +0530

    new

--- lib/animation.dart
+++ lib/animation.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:animated_text_kit/animated_text_kit.dart';
 
 class AnimationApp extends StatefulWidget {
   const AnimationApp({super.key});
@@ -84,13 +84,40 @@ class _AnimationAppState extends State<AnimationApp> with SingleTickerProviderSt
   ),
 
  ),
- body: Center(
- 
-  child: Container(
-    width: _animation.value,
-    height: _animation.value,
+ body: Column(
+  children: [Container(
+    width: 200,
+    height:200,
     color: Colors.blue,
-  ),
+  ), 
+  AnimatedTextKit(
+  animatedTexts: [
+    TypewriterAnimatedText(
+      'Good Morning',
+      textStyle: const TextStyle(
+        fontSize: 32.0,
+        fontWeight: FontWeight.bold,
+      ),
+      speed: const Duration(milliseconds: 200),
+    ),
+       FadeAnimatedText(
+      'Fade First',
+      textStyle: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+    ),
+    ScaleAnimatedText(
+      'Then Scale',
+      textStyle: TextStyle(fontSize: 70.0, fontFamily: 'Canterbury'),
+    ),
+  ],
+  
+  totalRepeatCount: 4,
+  pause: const Duration(milliseconds: 100),
+  displayFullTextOnTap: true,
+  stopPauseOnTap: true,
+),
+
+
+  ]
  ),
 
 
